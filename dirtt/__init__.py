1395af95599c509cc3a17212405a44cd2ea75bba
Robert Moggach <rob@moggach.com>
2011-07-17 14:43:06 -0400

    v0.1.4b2

--- dirtt/__init__.py
+++ dirtt/__init__.py
@@ -13,8 +13,8 @@ with internal methods that read,parse,render,and execute builds of
 user defined XML directory tree templates.
 """
 
-#v0.1.4b1
-VERSION = (0, 1, 4, 'beta', 1)
+#v0.1.4b2
+VERSION = (0, 1, 4, 'beta', 2)
 
 STATUSES = {'alpha': 'a', 'beta': 'b', 'releasecandidiate': 'rc' }
 
